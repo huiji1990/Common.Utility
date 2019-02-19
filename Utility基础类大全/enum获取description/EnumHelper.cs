baa01a1d1cefe912d33f23b91355a6f88b8b902e
zhujianhui <zjnbzjh@163.com>
2019-02-19 11:28:56 +0800

    add: getting description from enum

---
+++ Utility基础类大全/enum获取description/EnumHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Common.Utilities
+{
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// 获取Enum Description
+        /// use: EnumHelper.GetDescription(MyEnum.Option);
+        /// </summary>
+        /// <param name="en"></param>
+        /// <returns></returns>
+        public static string GetDescription(Enum en)
+        {
+            Type type = en.GetType();
+
+            MemberInfo[] memInfo = type.GetMember(en.ToString());
+
+            if (memInfo != null && memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+
+            return en.ToString();
+        }
+
+    }
+
+}
